0675ba20b7af402f0f44dc82c39e5217f5b4c044
datnguyennnx <official.nguyendat@gmail.com>
2025-03-22 18:07:30 +0700

    chore: change logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,20 @@ export const metadata: Metadata = {
   title: "Ballista UI",
   description:
     "Ballista: A precision-engineered Rust API service that launches targeted volleys of concurrent requests to stress-test web applications, delivering detailed performance metrics with the accuracy of its ancient namesake weapon.",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -22,6 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head />
       <body className={`${spaceMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
